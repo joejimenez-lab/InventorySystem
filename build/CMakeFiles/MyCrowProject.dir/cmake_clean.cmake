f7f5066bdfd61240e1189c22599dd6be58476b67
Nathan-Albano <natealbano123@gmail.com>
2025-01-14 12:16:52 -0800

    making sure crow works

---
+++ build/CMakeFiles/MyCrowProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCrowProject.dir/src/main.cpp.obj"
+  "CMakeFiles/MyCrowProject.dir/src/main.cpp.obj.d"
+  "MyCrowProject.exe"
+  "MyCrowProject.exe.manifest"
+  "MyCrowProject.pdb"
+  "libMyCrowProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyCrowProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
