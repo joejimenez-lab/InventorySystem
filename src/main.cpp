fae4bc084b87a546aaa08ba17dc5db56c745c511
Nathan-Albano <natealbano123@gmail.com>
2025-01-20 23:46:07 -0800

    working on registration

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <sstream>
 #include <unordered_map>
 #include <iostream>
-#include <string>
+#include <string>\
+#include <crow/query_string.h>
+
 
 std::string load_html(const std::string& file_path) {
     std::ifstream html_file(file_path);
@@ -28,6 +30,8 @@ std::unordered_map<std::string, std::string> parse_form_data(const std::string& 
     
     return form_data;
 }
+
+//Need to create url decoder for @ and stuff
 
 std::map<std::string, std::string> parse_urlencoded(const std::string& body) {
     std::map<std::string, std::string> result;
@@ -68,12 +72,13 @@ int main() {
 
     std::string login_username;
     std::string login_password;
+    std::vector<std::string> registration_info;
 
     CROW_ROUTE(app, "/")([](){
         std::string html_content = load_html("../html_pages/login.html");
         return html_content;
     });
-    
+
     CROW_ROUTE(app, "/login").methods("POST"_method)([&login_username, &login_password](const crow::request& req) {
         auto form_data = parse_urlencoded(req.body);
 
@@ -91,6 +96,21 @@ int main() {
     CROW_ROUTE(app, "/registration.html")([](){
         std::string html_content = load_html("../html_pages/registration.html");
         return html_content;
+    });
+    
+    CROW_ROUTE(app, "/registration").methods("POST"_method)([&registration_info](const crow::request& req) {
+        auto form_data = parse_urlencoded(req.body);
+
+        registration_info.push_back(form_data["username"]);
+        registration_info.push_back(form_data["password"]);
+        registration_info.push_back(form_data["email"]);
+        registration_info.push_back(form_data["phone"]);
+
+        for(int i = 0; i < registration_info.size(); i++){
+            std::cout << registration_info.at(i) << std::endl;
+        }
+        return crow::response(200, "Form submitted successfully");
+
     });
 
     app.port(8080).multithreaded().run();
